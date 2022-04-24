5a81dad83588663be4cd5e98612ead135eac43be
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-04-23 23:59:03 -0600

    Refactored code and added unit test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,27 @@
 const request = require('request');
-const args = process.argv;
-const query = args[2];
 
-let url = `https://api.thecatapi.com/v1/breeds/search?q=${query}`;
-request(url, (error, response, body) => {
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  console.log(typeof body);
-  const data = JSON.parse(body);
-  console.log(typeof data);
+//Fetch description of provided cat breed.
+const fetchBreedDescription = function(breedName, callback) {
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      //Breed not found
+      if (data.length === 0) {
+        callback(null, "Breed not found");
+      }
 
-  //Breed not found
-  if (data.length === 0) {
-    console.log("Breed not found");
-  }
+      //Just display description of the response
+      for (let cat of data) {
+        let description = cat.description;
+        callback(null, description);
+      }
+    }
+  });
+};
 
-  //Just display description of the response
-  for (let cat of data) {
-    let description = cat.description;
-    console.log(description);
-  }
-});
+module.exports = {
+  fetchBreedDescription
+};
